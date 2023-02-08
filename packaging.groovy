873c9b7ec1b68636b6003e5b56fd183461e62975
henry <henryborchers@yahoo.com>
2023-02-08 09:47:29 -0600

    getProjectMetadataFromDistInfo has error handling for missing file

--- packaging.groovy
+++ packaging.groovy
@@ -1,5 +1,11 @@
 def getProjectMetadataFromDistInfo(){
-    def metadataFile = findFiles(excludes: '', glob: '*.dist-info/METADATA')[0]
+    def metadataFile
+    try{
+        metadataFile = findFiles(excludes: '', glob: '*.dist-info/METADATA')[0]
+    }catch(ArrayIndexOutOfBoundsException ex) {
+        error 'Unable to locate METADATA file for a dist-info folder in workspace. Are you sure you created it?'
+    }
+
     def package_metadata = readProperties interpolate: true, file: metadataFile.path
     echo """Metadata:
 
